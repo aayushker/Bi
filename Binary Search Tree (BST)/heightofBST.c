86ebe1bedce175b6a7501529b35eab94e5e30baa
aayushker <singhaayushker@gmail.com>
2023-12-17 23:17:14 +0530

    Did height of a BST

---
+++ Binary Search Tree (BST)/heightofBST.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for a node of BST
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert a node in BST
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    } else if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+// Function to find the height of BST
+int findHeight(struct Node* root) {
+    if (root == NULL) {
+        return -1;
+    }
+    int leftHeight = findHeight(root->left);
+    int rightHeight = findHeight(root->right);
+    return (leftHeight > rightHeight) ? leftHeight + 1 : rightHeight + 1;
+}
+
+int main() {
+    struct Node* root = NULL;
+    int data;
+
+    // Insert nodes into BST
+    root = insert(root, 10);
+    root = insert(root, 5);
+    root = insert(root, 15);
+    root = insert(root, 3);
+    root = insert(root, 7);
+    root = insert(root, 12);
+    root = insert(root, 18);
+
+    // Find the height of BST
+    int height = findHeight(root);
+    printf("Height of BST: %d\n", height);
+
+    return 0;
+}
